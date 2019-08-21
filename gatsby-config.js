18fc4a5a75281367d326f7773b6c3cca646d4f81
metafield <anthony.sylvester99@gmail.com>
2019-08-21 15:21:05 -0700

    Installs: TS plugin, adds some metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    name: `Tomb: Lock it down!`,
+    tagline: `Yoursource for hiding your perfectly legal activities!`,
+  },
+  plugins: [`gatsby-plugin-typescript`],
 }
