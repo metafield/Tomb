54da4ecf31f3e30fc994cfe9fdccc40658419e6a
metafield <anthony.sylvester99@gmail.com>
2019-08-21 15:40:03 -0700

    installs pre-fetch font plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,18 @@ module.exports = {
     name: `Tomb: Lock it down!`,
     tagline: `Yoursource for hiding your perfectly legal activities!`,
   },
-  plugins: [`gatsby-plugin-typescript`],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Livvic`,
+            variants: [`400`, `600`],
+          },
+        ],
+      },
+    },
+  ],
 }
