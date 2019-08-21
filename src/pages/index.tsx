2893e95b75851f7fbf314ca5c58150abd6ef9001
metafield <anthony.sylvester99@gmail.com>
2019-08-21 15:46:21 -0700

    Hello world.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Container>
+    <p>Hello</p>
+    <p>World</p>
+  </Container>
+)
 
-const Container = styled.div``
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  align-content: center;
+  grid-template: auto / 1fr 1fr;
+
+  > p {
+    text-align: center;
+    font: 3rem / 1.2rem 'Livvic', sans-serif;
+    font-weight: 600;
+  }
+`
