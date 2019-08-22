059a850852651f3291cf48c0c7eb5b5b426db24a
metafield <anthony.sylvester99@gmail.com>
2019-08-22 12:12:44 -0700

    adds logic for encryption

---
+++ src/components/encryption/Encryptor.ts
@@ -0,0 +1,55 @@
+export class Encryptor {
+  public alphabet = this.generateAlphabet()
+  public cipher = this.generateAlphabet()
+  public subTable: { [key: string]: string }
+
+  constructor(public input: string) {
+    this.scrambleCipher()
+    this.buildSubstituteTable()
+  }
+
+  public encrypt(): string {
+    const cleanedText = this.input.toLocaleLowerCase()
+    const encryptedText = [...cleanedText].map((char, index): string => {
+      const code = cleanedText.charCodeAt[index]
+      if (code >= 97 && code < code + 26) {
+        return this.subChar(char)
+      } else {
+        return char
+      }
+    })
+
+    return encryptedText.join()
+  }
+
+  private generateAlphabet(): string[] {
+    console.log('in gen alpha')
+
+    return new Array<String>(26).fill('').map((_, index): string => {
+      const firstLetterCode = 97
+      return String.fromCharCode(firstLetterCode + index)
+    })
+  }
+
+  private scrambleCipher(): void {
+    console.log('in scramble')
+
+    const randomIndex = (): number => Math.floor(Math.random() * 26)
+
+    this.cipher.forEach((char, index, array) => {
+      const destIndex = randomIndex()
+      array[index] = array[destIndex]
+      array[destIndex] = char
+    })
+  }
+
+  private buildSubstituteTable() {
+    this.alphabet.forEach((char, index) => {
+      this.subTable[char] = this.cipher[index]
+    })
+  }
+
+  private subChar(char: string): string {
+    return this.subTable[char]
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 import '../styles/global.css'
-import { Encryptor } from '../components/encryptor/encryptor'
+import { Encryptor } from '../components/encryption/encryptor'
+import { Encryption } from '../components/encryption/Encryption'
+
+const tomb = new Encryptor()
 
 export default () => (
   <Container>
-    <Encryptor />
+    <p>
+      {tomb.alphabet}->{tomb.cipher}
+    </p>
+    <Encryption />
   </Container>
 )
 
 const Container = styled.div`
   display: grid;
   height: 100vh;
-  grid-template: 1fr / 50px 1fr 50px;
+  grid-template-columns: 50px 1fr 50px;
+
+  > p {
+    grid-column: 2;
+    border: solid cyan 1px;
+  }
 `
